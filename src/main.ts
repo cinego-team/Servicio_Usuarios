2608c7056b9b14e829c8fa4df17574cb01d76aaf
Ramiro Lynch <ramirolynch@hotmail.com>
2025-11-11 23:35:15 -0300

    fix puerto

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.enableCors({ origin: 'http://localhost:4200', credentials: true });
-  await app.listen(3000);
+    const app = await NestFactory.create(AppModule);
+    await app.enableCors({ origin: 'http://localhost:3000', credentials: true });
+    await app.listen(3004);
 }
 bootstrap();
