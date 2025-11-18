a3f67077aa34a834b919ae1fb99523a53ccd430e
Gianella Rocha <gianellarochavendivengo123@gmail.com>
2025-11-18 15:52:28 -0300

    cambios en el dto para validar email

--- src/dto/login.dto.ts
+++ src/dto/login.dto.ts
@@ -1,4 +1,8 @@
+import { IsEmail, IsNotEmpty } from 'class-validator';
 export class LoginDTO {
+    @IsEmail({}, { message: 'Email inválido.' })
     email: string;
+
+    @IsNotEmpty({ message: 'La contraseña es obligatoria.' })
     password: string;
 }
--- src/dto/register.dto.ts
+++ src/dto/register.dto.ts
@@ -1,22 +1,35 @@
-import { IsString, Matches } from 'class-validator';
+import {
+    IsString,
+    Matches,
+    IsEmail,
+    IsNotEmpty,
+    MinLength,
+} from 'class-validator';
 
 export class RegisterDTO {
+    @IsNotEmpty({ message: 'El nombre es obligatorio.' })
+    @MinLength(2, { message: 'El nombre debe tener al menos 2 caracteres.' })
     nombre: string;
+    @IsNotEmpty({ message: 'El apellido es obligatorio.' })
+    @MinLength(3, { message: 'El apellido debe tener al menos 3 caracteres.' })
     apellido: string;
+    @IsNotEmpty({ message: 'El email es obligatorio.' })
+    @IsEmail({}, { message: 'Email inválido.' })
     email: string;
-  @IsString()
-  // 1) Sin espacios al inicio ni al final (equivale a password !== password.trim())
-  @Matches(/^\S.*\S$/, {
-    message: 'La contraseña no puede tener espacios al inicio ni al final',
-  })
-  // 2) Mismo regex que tu función: mayúscula, número, especial, mínimo 8
-  @Matches(/^(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z0-9]).{8,}$/, {
-    message:
-      'La contraseña debe tener al menos 8 caracteres, una mayúscula, un número y un carácter especial',
-  })
-  contrasena: string;
+    @IsString()
+    // 1) Sin espacios al inicio ni al final (equivale a password !== password.trim())
+    @Matches(/^\S.*\S$/, {
+        message: 'La contraseña no puede tener espacios al inicio ni al final',
+    })
+    // 2) Mismo regex que tu función: mayúscula, número, especial, mínimo 8
+    @Matches(/^(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z0-9]).{8,}$/, {
+        message:
+            'La contraseña debe tener al menos 8 caracteres, una mayúscula, un número y un carácter especial',
+    })
+    contrasena: string;
     dd: number;
     mm: number;
     aaaa: number;
+    @IsNotEmpty({ message: 'El teléfono es obligatorio.' })
     nroTelefono: string;
 }
