9a6ae536873dea1a37f24d95ef2b141e62c7db83
Ramiro Lynch <ramirolynch@hotmail.com>
2026-02-07 20:33:15 -0300

    fix urls permitidas cors

--- src/main.ts
+++ src/main.ts
@@ -5,13 +5,13 @@ async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     await app.enableCors({
         origin: [
-            'https://api-gateway-one-rose.vercel.app',
-            'https://servicio-integracion-mercado-pago.vercel.app',
-            'https://microservicio-de-peliculas.vercel.app',
-            'https://servicio-promociones.vercel.app',
-            'https://servicio-ventas-seven.vercel.app',
-            'https://servicio-funciones-y-salas.vercel.app',
-            'https://servicioenvioemails.vercel.app'
+            'https://apigateway-v5pv.onrender.com',
+            'https://servicio-integracion-mercadopago.onrender.com',
+            'https://microservicio-de-peliculas.onrender.com',
+            'https://serviciopromociones-wftn.onrender.com',
+            'https://servicioventas.onrender.com',
+            'https://servicio-funciones-y-salas.onrender.com',
+            'https://servicio-envio-emails.onrender.com'
         ],
         methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
         credentials: true
