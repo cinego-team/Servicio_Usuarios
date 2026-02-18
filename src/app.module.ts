bfe008abe33d688ba20b321eda7aed2c8ea572cf
Ramiro Lynch <ramirolynch@hotmail.com>
2026-02-18 19:40:59 -0300

    fix app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,10 +10,17 @@ import { RoleEntity } from "./entities/role.entity";
 import { UserEntity } from "./entities/user.entity";
 import { TipoClienteModule } from './tipo-cliente/tipo-cliente.module';
 import { TipoClienteEntity } from "./entities/tipoCliente.entity";
+import { ConfigModule } from '@nestjs/config';
+
 
 @Module({
     imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+        }),
         TypeOrmModule.forRoot({
+            type: 'postgres',
+            host: 'localhost',
             port: +process.env.PUERTO_BD,
             database: process.env.PG_DATABASE_MS_USUARIOS,
             username: process.env.PG_USERNAME,
